e157a8b661a961123f22ffaa2fe3cb050ae7c3ba
heyotv <hey.tumilovich@gmail.com>
2020-02-24 12:18:34 +0300

    feat: add solution of count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,9 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let count = 0;
+  matrix.forEach(function(arr) {
+    arr.forEach(function(item) {
+      item === '^^' ? count++ : false;
+    });
+  });
+  return count;
 };
