d586e22efd558cdfd1789199d3c4f431530b33f0
heyotv <hey.tumilovich@gmail.com>
2020-02-27 13:45:10 +0300

    feat: add solution of vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,58 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+
+    constructor(reverse) {
+        this.reverse = reverse;
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    encrypt(str, key) {
+        let primary = 'A'.charCodeAt();
+        let result = [];
+        let keyIndexCount = 0;
+        str = str.toUpperCase().split('');
+        key = key.toUpperCase().split('');
+
+        for (let i = 0; i < str.length; i++) {
+            if (!str[i].match(/[A-Z]/)) {
+                result.push(str[i]);
+                continue;
+            }
+
+            let encKey = (str[i].charCodeAt() + key[keyIndexCount].charCodeAt()) % 26;
+            result.push(String.fromCharCode(encKey + primary));
+            keyIndexCount++;
+
+            if (keyIndexCount >= key.length) {
+                keyIndexCount = 0;
+            }
+
+        }
+
+        return (this.reverse === false) ? result.reverse().join('') : result.join('');  
+    }
+
+    decrypt(str, key) {
+        let primary = 'A'.charCodeAt();
+        let result = [];
+        let keyIndexCount = 0;
+        str = str.toUpperCase().split('');
+        key = key.toUpperCase().split('');
+
+        for (let i = 0; i < str.length; i++) {
+            if (!str[i].match(/[A-Z]/)) {
+                result.push(str[i]);
+                continue;
+            }
+
+            let decKey = (str[i].charCodeAt() + 26 - key[keyIndexCount].charCodeAt()) % 26;
+            result.push(String.fromCharCode(decKey + primary));
+            keyIndexCount++;
+
+            if (keyIndexCount >= key.length) {
+                keyIndexCount = 0;
+            }
+        }
+
+        return (this.reverse === false) ? result.reverse().join('') : result.join('');     
     }
 }
 
